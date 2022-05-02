dd9fbd67b1ce4c092ad9b93cd17ad1acdd0ddf1b
Motya22 <motya2222@gmail.com>
2022-05-02 14:04:16 +0300

    complete task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(+sampleActivity) || +sampleActivity <= 0 || +sampleActivity > 15) {
+    return false;
+  }
+
+  const N0DividedByN = MODERN_ACTIVITY / sampleActivity;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(N0DividedByN) / k);
+
+  return t;
 }
 
 module.exports = {
